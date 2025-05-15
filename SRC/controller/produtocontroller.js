8e581abcd5300fab4bcb27bcb3c7cea81be3069a
mands-oc <mands.oliveira552@gmail.com>
2025-05-15 11:52:53 -0300

    Corrigindo erros da API.

--- SRC/controller/produtocontroller.js
+++ SRC/controller/produtocontroller.js
@@ -7,33 +7,36 @@ const produtoSchema = joi.object({
     valorUnit: joi.string().required(),
     estoque: joi.string().required(),
     imagem: joi.string().allow().max(200),
-    idCategoria: joi.string().required()
+    idCategoria: joi.string()
 });
 
+//Listar todos os produtos
 exports.listarProdutos = async (req, res) => {
     try {
         const [result] = await db.query('SELECT * FROM produto');
         res.json(result);
     } catch (err) {
-        console.error('Erro ao buscar produto:', err);
-        res.status(500).json({ error: 'Erro interno do servidor' })
+        console.error('Erro ao buscar produtos:', err);
+        res.status(500).json({ error: 'Erro interno do servidor' });
     }
 };
 
+//Buscar um produto por ID
 exports.listarProdutoID = async (req, res) => {
     const { idProduto } = req.params;
     try {
         const [result] = await db.query('SELECT * FROM produto WHERE idProduto = ?', [idProduto]);
         if (result.length === 0) {
-            return res.status(404).json({ error: 'Produto não encontrado' })
+            return res.status(404).json({ error: 'Produto não encontrado' });
         }
-        res.json(result[0])
+        res.json(result[0]);
     } catch (err) {
         console.error('Erro ao buscar produto:', err);
-        res.status(500).json({ error: 'Erro interno do servidor' })
+        res.status(500).json({ error: 'Erro interno do servidor' });
     }
 };
 
+//Buscar produtos por nome
 exports.buscarProdutoNome = async (req, res) => {
     const { nomeProduto } = req.params;
     try {
@@ -48,26 +51,28 @@ exports.buscarProdutoNome = async (req, res) => {
     }
 };
 
+//Adicionar um novo produto
 exports.adicionarProduto = async (req, res) => {
     const { nomeProduto, descricao, valorUnit, estoque, imagem, idCategoria } = req.body;
     const { error } = produtoSchema.validate({ nomeProduto, descricao, valorUnit, estoque, imagem, idCategoria });
     if (error) {
-        return res.status(400).json({ error: error.details[0].message })
+        return res.status(400).json({ error: error.details[0].message });
     }
     try {
         const novoProduto = { nomeProduto, descricao, valorUnit, estoque, imagem, idCategoria };
         await db.query('INSERT INTO produto SET ?', novoProduto);
         res.json({ message: 'Produto adicionado com sucesso' });
     } catch (err) {
-        console.error('Erro ao adicionar produto: ', err)
-        res.status(500).json({ error: 'Erro ao adicionar produto' })
+        console.error('Erro ao adicionar produto: ', err);
+        res.status(500).json({ error: 'Erro ao adicionar produto' });
     }
 };
 
+//Atualizar um produto
 exports.atualizarProduto = async (req, res) => {
     const { idProduto } = req.params;
-    const { nomeProduto, descricao, valorUnit, estoque, imagem, idCategoria } = req.body;
-    const { error } = produtoSchema.validate({ nomeProduto, descricao, valorUnit, estoque, imagem, idCategoria });
+    const { nomeProduto, descricao, valorUnit, estoque, imagem} = req.body;
+    const { error } = produtoSchema.validate({ nomeProduto, descricao, valorUnit, estoque, imagem});
     if (error) {
         return res.status(400).json({ error: error.details[0].message });
     }
@@ -76,7 +81,7 @@ exports.atualizarProduto = async (req, res) => {
         if (result.length === 0) {
             return res.status(404).json({ error: 'Produto não encontrado' });
         }
-        const produtoAtualizado = { nomeProduto, descricao, valorUnit, estoque, imagem, idCategoria };
+        const produtoAtualizado = { nomeProduto, descricao, valorUnit, estoque, imagem};
         await db.query('UPDATE produto SET ? WHERE idProduto = ? ', [produtoAtualizado, idProduto]);
         res.json({ message: 'Produto atualizado com sucesso' });
     } catch (err) {
@@ -85,6 +90,7 @@ exports.atualizarProduto = async (req, res) => {
     }
 };
 
+//Deletar um produto
 exports.deletarProduto = async (req, res) => {
     const { idProduto } = req.params;
     try {
